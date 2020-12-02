1a260fae87263ffd309b2807ea0bc1bbe62a0469
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-12-02 12:02:06 +0600

    es6 let vs const vs var difference

---
+++ letVsConstvsVar.js
@@ -0,0 +1,21 @@
+function mehedi(){
+    //block 1
+    var a = 23 // assignable from anywhere
+    { // block 2
+        let b = 20  // assignable anywhere in block 2 and block 3 :: it a block level variable
+        { //block 3
+            const b = 10 // impossible to assign again 
+            console.log(b);
+            console.log(a);
+
+        }
+        b = 15
+        console.log(b);
+
+    }
+    console.log(b);
+    
+   
+}
+
+mehedi()
