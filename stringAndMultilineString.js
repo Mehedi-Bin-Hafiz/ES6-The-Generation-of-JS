3c6357d18bd034ed9325b86d0be29a713a788fc8
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-12-02 12:17:51 +0600

    Introduction to back tick

---
+++ stringAndMultilineString.js
@@ -0,0 +1,18 @@
+const name = 'mehedi'
+const age = '121'
+const email = 'mehedibinhafiz@gmail.com'
+
+const details = 'Name: ' + name + ' Age: ' + age + 'Email: '+ email
+
+console.log(details);
+
+// es6
+
+// `` back tick
+const newdetails = `
+Name: ${name}
+age: ${age}
+email: ${email}
+
+`
+console.log(newdetails);
