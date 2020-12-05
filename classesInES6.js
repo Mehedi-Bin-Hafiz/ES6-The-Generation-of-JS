834304e4b317601f82afecd998e3c78175548167
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-12-05 21:12:48 +0600

    Unsolvable class concept in js

---
+++ classesInES6.js
@@ -0,0 +1,13 @@
+class Person{
+    constructor(name, email){
+        this.name = name
+        this.email = email
+    
+    }
+
+    print(){
+        console.log(`name:${this.name}`)
+    }
+}
+
+export default Person;
